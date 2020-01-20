fee5f832de10a21b1456915074e6c4665a64a0e1
JAYA-16 <59077978+JAYA-16@users.noreply.github.com>
2020-01-20 19:46:01 +0530

    Gify API using react

--- App.js
+++ App.js
@@ -1,12 +1,90 @@
-import React, { Component } from "react";
+import React, { useEffect, Component } from "react";
+import GifList from "./components/GifList";
+import SearchBar from "./components/SearchBar";
+import request from "superagent";
+//import { throttle, debounce } from "throttle-debounce";
+//import InfiniteScroll from "react-infinite-scroller";
 import "./App.css";
-import Form from "./components/form";
 
-class App extends Component {
+export class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      gifs: [],
+      flag: 1
+    };
+
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+  //var flag=1;
+  handleScroll() {
+    const windowHeight =
+      "innerHeight" in window
+        ? window.innerHeight
+        : document.documentElement.offsetHeight;
+
+
+    const body = document.body;
+    const html = document.documentElement;
+    
+    const docHeight = Math.max(
+      body.scrollHeight,
+      body.offsetHeight,
+      html.clientHeight,
+      html.scrollHeight,
+      html.offsetHeight
+    );
+    const windowBottom = windowHeight + window.pageYOffset;
+    //console.log("win", windowBottom);
+
+    //console.log("doc", docHeight);
+
+    if (windowBottom + 1 >= docHeight) {
+      console.log("reached!!!!");
+      this.setState({ flag: this.state.flag + 1 });
+    } else {
+      console.log("not yet!!!");
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleTermChange(term) {
+    const url = `http://api.giphy.com/v1/gifs/search?q=${term}&api_key=Gc7131jiJuvI7IdN0HZ1D7nh0ow5BU6g&limit=25`;
+
+    request.get(url, (err, res) => {
+      this.setState({ gifs: res.body.data });
+    });
+  }
   render() {
+    const child = [];
+    for (var i = 0; i < this.state.flag; i += 1) {
+      child.push(<GifList gifs={this.state.gifs} />);
+    }
     return (
-      <div className="App">
-        <Form />
+      <div id="main1">
+        <div className="search">
+          <h1>
+            <img
+              src="https://static.ezgif.com/images/format-demo/butterfly.png"
+              alt=""
+            />
+            Gify Search
+          </h1>
+        </div>
+        <SearchBar onTermChange={term => this.handleTermChange(term)} />
+
+        <div id="display">
+          {/* <GifList gifs={this.state.gifs} /> */}
+          {child}
+        </div>
       </div>
     );
   }
