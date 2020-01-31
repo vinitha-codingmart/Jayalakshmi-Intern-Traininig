5f2cc7f75deb9f713722f95c8972047c9f886f5f
JAYA-16 <59077978+JAYA-16@users.noreply.github.com>
2020-01-31 17:41:35 +0530

    progress bar added

--- todoItem.js
+++ todoItem.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./todoItem.css";
+import InputRange from "react-input-range";
+import _ from "lodash";
+import uuid from "uuid/v4";
+import "react-input-range/lib/css/index.css";
 
 export default class TodoItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "",
-      array: []
+      name: "Name",
+      array: [{ id: uuid(), keyTerm: "Buy an Online course", value: 0 }],
+      value: 0
     };
   }
   handleNameChange = event => {
@@ -15,13 +20,55 @@ export default class TodoItem extends React.Component {
     });
   };
 
-  handleSubmit = event => {
-    this.state.array.push(this.state.name);
+  handleSlider = event => {
     this.setState({
-      array: this.state.array,
-      name: ""
+      value: event,
+      value0: event / 2
     });
   };
+  handleSubmit = event => {
+    const newKeyTerm = { id: uuid(), keyTerm: this.state.name, value: 0 };
+    this.setState(prevState => ({
+      ...prevState,
+      name: "",
+      array: [...prevState.array, newKeyTerm]
+    }));
+
+    // fetch("http://localhost:7000/addkeyterms", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json"
+    //   },
+    //   body: JSON.stringify({
+    //     title: this.props.todo.text,
+    //     subtopics: this.state.name
+    //   })
+    // })
+    //   .then(res => res.json())
+    //   .then(data => {
+    //     this.setState({
+    //       sample: data
+    //     });
+    //   });
+
+    // this.setState({
+    //   array: this.state.array,
+    //   name: ""
+    // });
+  };
+
+  handleKeyTermValueChange(e, id) {
+    const newKeyTerms = this.state.array.slice();
+    const index = _.findIndex(newKeyTerms, { id });
+    newKeyTerms[index].value = e;
+    this.setState({ array: newKeyTerms });
+  }
+
+  computeAverage() {
+    const sum = _.sumBy(this.state.array, "value");
+    const average = sum / this.state.array.length;
+    this.setState({ value: average });
+  }
 
   removeTodo(id) {
     this.props.removeTodo(id);
@@ -30,21 +77,40 @@ export default class TodoItem extends React.Component {
   render() {
     return (
       <div className="todoWrapper">
-        <h1> {this.props.todo.text}</h1>
-
-        <br />
-        <br />
+        <h2>
+          {" "}
+          {this.props.todo.text}{" "}
+          <form className="form">
+            <InputRange
+              maxValue={100}
+              minValue={0}
+              value={this.state.value}
+              onChange={() => null}
+              // onChangeComplete={value0 => console.log(value0)}
+            />
+          </form>
+        </h2>
         <div>
           {this.state.array.map(i => {
             return (
-              <div>
-                <li>{i}</li>
+              <div key={i.id}>
+                <li>
+                  <h4>{i.keyTerm}</h4>
+                </li>
+                <form className="form">
+                  <InputRange
+                    maxValue={100}
+                    minValue={0}
+                    value={i.value}
+                    onChange={e => this.handleKeyTermValueChange(e, i.id)}
+                    onChangeComplete={this.computeAverage.bind(this)}
+                  />
+                </form>
               </div>
             );
           })}
         </div>
-        <br />
-        <br />
+
         <input
           type="text"
           value={this.state.name}
